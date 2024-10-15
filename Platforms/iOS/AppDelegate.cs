4ee4cf253700da7dfba763de6a951398161543fd
CJKorn <cjkornie@gmail.com>
2024-10-15 14:40:26 +1100

    Weather app done good job everyone!!!

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace Blaze {
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
