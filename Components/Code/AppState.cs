dd329180121d6e57e504f1a89fb9e4293fd93a5b
AlyssaWestwood <Alyssa.Westwood@student.uts.edu.au>
2024-10-27 20:35:25 +1100

    comments done on AppState page

--- Components/Code/AppState.cs
+++ Components/Code/AppState.cs
@@ -2,10 +2,13 @@ using System;
 using System.ComponentModel;
 
 public class AppState : INotifyPropertyChanged
+    //defines the public class called AppState
+    //INotifyPropertyChanged interface is implemented by AppState class
+    //INotifyPropertyChanged is used to notify when a value within AppState is changed
 {
     private bool _hosting;
-
-    public bool Hosting
+    //the method below, is used to determine if the status of _hosting changes. if this is the case, the NotifyPropertyChanged method is called
+    public bool Hosting 
     {
         get => _hosting;
         set
@@ -19,7 +22,8 @@ public class AppState : INotifyPropertyChanged
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
-
+    //the method below triggers the property changed event within the current instance of the class.
+    //a notification is sent using this method, using the propertyName string, which refers to the property that has changed.
     private void NotifyPropertyChanged(string propertyName)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
