d770ad3c37368109cfef8ebfcdedc2b4ff8faded
T Thornton-Bott <Toby.J.Thornton-Bott@student.uts.edu.au>
2024-10-27 22:18:27 +1100

    Polygon colours

--- Components/Code/Markers/Mono.cs
+++ Components/Code/Markers/Mono.cs
@@ -6,9 +6,12 @@ public class Mono : Report {
 	public double Lat { get; set; }
 	public double Lng { get; set; }
 
+	
+
 	public Mono(string name, string description, double lat, double lng, string icon, Category cat) : base(name, description, icon, cat) {
 		Lat = lat;
 		Lng = lng;
+
 		//Cat = cat;
 	}
 }
