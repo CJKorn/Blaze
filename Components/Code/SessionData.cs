b31e7e8896b9520dcb5ee891cef9ddc14ab59c7f
AlyssaWestwood <Alyssa.Westwood@student.uts.edu.au>
2024-10-27 20:45:16 +1100

    comments to session data done and appData finalised too

--- Components/Code/SessionData.cs
+++ Components/Code/SessionData.cs
@@ -1,13 +1,16 @@
 ﻿using System.Text.Json;
 using System.IO;
 using GoogleMapsComponents.Maps;
-
+//this class is used to declare various data elements that can be read and modified from different locations, and it also has the file paths for the .JSON 
+//and the API file.
 internal static class SessionData
 {
+    //below code declares static string data elements that correspond with the file path for the .JSON file and the API file.
     public static string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
     public static string jsonFilePath = Path.Combine(path, "Blaze/data.json");
     public static string apiFilePath = Path.Combine(path, "Blaze/keys.apikeys");
-
+    //below code declares various data elements and the getters and setters for them
+    //by stating get; set; it means that the getters and setters are default, and the elements can be read and modified from anywhere that can access SessionData
     public static bool signedIn { get; set; }
     public static bool hosting { get; set; }
     public static string myIP { get; set; }
