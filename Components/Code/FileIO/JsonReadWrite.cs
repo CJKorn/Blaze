e6ec2f5ff4c3ba1c771a5646441010ac6ea9d923
CJKorn <cjkornie@gmail.com>
2024-10-27 20:41:21 +1100

    Fix bugs in validation of IP and User sign in

--- Components/Code/FileIO/JsonReadWrite.cs
+++ Components/Code/FileIO/JsonReadWrite.cs
@@ -5,60 +5,66 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 public class JsonReadWrite : IReadWrite {
-	private JsonSerializerOptions GetSerializerOptions() {
-		var options = new JsonSerializerOptions {
-			WriteIndented = true,
-			DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-		};
-		return options;
-	}
+    private JsonSerializerOptions GetSerializerOptions() {
+        var options = new JsonSerializerOptions {
+            WriteIndented = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        };
+        return options;
+    }
 
-	public void SerializeFile<T>(List<T> data, string filePath) {
-		var options = GetSerializerOptions();
-		string json = JsonSerializer.Serialize(data, options);
-		File.WriteAllText(filePath, json);
-	}
+    public void SerializeFile<T>(List<T> data, string filePath) {
+        var options = GetSerializerOptions();
+        string json = JsonSerializer.Serialize(data, options);
+        File.WriteAllText(filePath, json);
+    }
 
-	public string SerializeString<T>(List<T> data) {
-		var options = GetSerializerOptions();
-		return JsonSerializer.Serialize(data, options);
-	}
+    public string SerializeString<T>(List<T> data) {
+        var options = GetSerializerOptions();
+        return JsonSerializer.Serialize(data, options);
+    }
 
-	public List<T> DeserializeFile<T>(string filePath) {
-		if (!File.Exists(filePath)) {
-			return new List<T>();
-		}
-		if (string.IsNullOrEmpty(File.ReadAllText(filePath))) {
-			return new List<T>();
-		}
+    public List<T> DeserializeFile<T>(string filePath) {
+        if (!File.Exists(filePath)) {
+            return new List<T>();
+        }
 
-		string json = File.ReadAllText(filePath);
-		return DeserializeString<T>(json);
-	}
+        string json = File.ReadAllText(filePath);
+        if (string.IsNullOrEmpty(json)) {
+            return new List<T>();
+        }
 
-	public List<T> DeserializeString<T>(string json) {
-		if (string.IsNullOrEmpty(json)) {
-			return new List<T>();
-		}
+        return DeserializeString<T>(json);
+    }
 
-		var options = GetSerializerOptions();
-		var jsonArray = JsonDocument.Parse(json).RootElement;
-		var result = new List<T>();
+    public List<T> DeserializeString<T>(string json) {
+        if (string.IsNullOrEmpty(json)) {
+            return new List<T>();
+        }
 
-		foreach (var element in jsonArray.EnumerateArray()) {
-			var type = element.GetProperty("$type").GetString();
-			switch (type) {
-				case "mono":
-					result.Add((T)(object)JsonSerializer.Deserialize<Mono>(element.GetRawText(), options));
-					break;
-				case "poly":
-					result.Add((T)(object)JsonSerializer.Deserialize<Poly>(element.GetRawText(), options));
-					break;
-				default:
-					throw new NotSupportedException($"Type {type} is not supported");
-			}
-		}
+        try {
+            var options = GetSerializerOptions();
+            var jsonArray = JsonDocument.Parse(json).RootElement;
+            var result = new List<T>();
 
-		return result;
-	}
+            foreach (var element in jsonArray.EnumerateArray()) {
+                var type = element.GetProperty("$type").GetString();
+                switch (type) {
+                    case "mono":
+                        result.Add((T)(object)JsonSerializer.Deserialize<Mono>(element.GetRawText(), options));
+                        break;
+                    case "poly":
+                        result.Add((T)(object)JsonSerializer.Deserialize<Poly>(element.GetRawText(), options));
+                        break;
+                    default:
+                        throw new NotSupportedException($"Type {type} is not supported");
+                }
+            }
+
+            return result;
+        }
+        catch (JsonException ex) {
+            return new List<T>();
+        }
+    }
 }
