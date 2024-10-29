9223263ec894d4709def48e8ab9dfc3559ad6bdf
CJKorn <cjkornie@gmail.com>
2024-10-29 12:16:32 +1100

    Stats page works maybe

--- Components/Code/Stat.cs
+++ Components/Code/Stat.cs
@@ -1,8 +1,9 @@
 ﻿internal static class Stat {
-    private static readonly Dictionary<int, int> hourlyCounts = new Dictionary<int, int>();
     public static int users { get; set; } = 0;
     public static int usersOutput { get; set; } = 0;
     private static Timer timer;
+    private static readonly Dictionary<int, int> getRequestCounts = new Dictionary<int, int>();
+    private static readonly Dictionary<int, int> reportUploadCounts = new Dictionary<int, int>();
 
     public static void Start() {
         timer = new Timer(UpdateUsers, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
@@ -10,26 +11,38 @@
 
     public static void UpdateUsers(object state) {
         usersOutput = users;
-        users = 0;
-    }
-
-    public static void IncrementHourCount(int hour) {
-        if (hourlyCounts.ContainsKey(hour)) {
-            hourlyCounts[hour]++;
-        } else {
-            hourlyCounts[hour] = 1;
-        }
-    }
-
-    public static int GetHourCount(int hour) {
-        return hourlyCounts.ContainsKey(hour) ? hourlyCounts[hour] : 0;
+        users = 10;
     }
 
     public static void ReportUpload() {
-        //
+        int currentHour = GetCurrentHour();
+        if (reportUploadCounts.ContainsKey(currentHour)) {
+            reportUploadCounts[currentHour]++;
+        } else {
+            reportUploadCounts[currentHour] = 1;
+        }
     }
 
     public static void GetReq() {
-        //
+        int currentHour = GetCurrentHour();
+        if (getRequestCounts.ContainsKey(currentHour)) {
+            getRequestCounts[currentHour]++;
+        } else {
+            getRequestCounts[currentHour] = 1;
+        }
+    }
+
+    public static int GetCurrentHour() {
+        TimeSpan timeSpan = DateTime.Now - DateTime.MinValue;
+        int totalHours = (int)timeSpan.TotalDays * 24 + DateTime.Now.Hour;
+        return totalHours;
+    }
+
+    public static int GetGetRequestCount(int hour) {
+        return getRequestCounts.ContainsKey(hour) ? getRequestCounts[hour] : 0;
+    }
+
+    public static int GetReportUploadCount(int hour) {
+        return reportUploadCounts.ContainsKey(hour) ? reportUploadCounts[hour] : 0;
     }
 }
