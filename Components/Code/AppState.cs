43cee8e48cf2f1398ea904a1fa5b9e464886c783
CJKorn <cjkornie@gmail.com>
2024-10-29 11:23:05 +1100

    Update map on reading of reports

--- Components/Code/AppState.cs
+++ Components/Code/AppState.cs
@@ -1,31 +1,44 @@
 using System;
 using System.ComponentModel;
+using System.Collections.Generic;
+
 //this classed is used to keep track of the hosting status of the application and send out an alert if the hosting status changes.
 public class AppState : INotifyPropertyChanged
-    //defines the public class called AppState
-    //INotifyPropertyChanged interface is implemented by AppState class
-    //INotifyPropertyChanged is used to notify when a value within AppState is changed
+//defines the public class called AppState
+//INotifyPropertyChanged interface is implemented by AppState class
+//INotifyPropertyChanged is used to notify when a value within AppState is changed
 {
-    private bool _hosting;
-    //the method below, is used to determine if the status of _hosting changes. if this is the case, the NotifyPropertyChanged method is called
-    public bool Hosting 
-    {
-        get => _hosting;
-        set
-        {
-            if (_hosting != value)
-            {
-                _hosting = value;
-                NotifyPropertyChanged(nameof(Hosting));
-            }
-        }
-    }
+	private bool _hosting;
+	private List<Report> _reports;
 
-    public event PropertyChangedEventHandler PropertyChanged;
-    //the method below triggers the property changed event within the current instance of the class.
-    //a notification is sent using this method, using the propertyName string, which refers to the property that has changed.
-    private void NotifyPropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+	//the method below, is used to determine if the status of _hosting changes. if this is the case, the NotifyPropertyChanged method is called
+	public bool Hosting {
+		get {
+			NotifyPropertyChanged(nameof(Hosting));
+			return _hosting;
+		}
+		set {
+			_hosting = value;
+			NotifyPropertyChanged(nameof(Hosting));
+		}
+	}
+
+	//the method below, is used to determine if the status of _reports changes. if this is the case, the NotifyPropertyChanged method is called
+	public List<Report> Reports {
+		get {
+			NotifyPropertyChanged(nameof(Reports));
+			return _reports;
+		}
+		set {
+			_reports = value;
+			NotifyPropertyChanged(nameof(Reports));
+		}
+	}
+
+	public event PropertyChangedEventHandler PropertyChanged;
+	//the method below triggers the property changed event within the current instance of the class.
+	//a notification is sent using this method, using the propertyName string, which refers to the property that has changed.
+	private void NotifyPropertyChanged(string propertyName) {
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
 }
